442c3121c1af7d9757fd25c753ae5f2c741c3456
Nick Ferdman <ferdman.inc@gmail.com>
2020-08-19 20:55:14 +0300

    Add func that gets user's ids by their screen_name Add func that make requests to keep code dry

--- friends_paths.py
+++ friends_paths.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from collections import deque
 from requests.exceptions import HTTPError
 from dotenv import load_dotenv
+from time import sleep
 
 
 def main():
@@ -14,8 +15,12 @@ def main():
     load_dotenv()
     access_token = os.getenv('API_TOKEN')  # use service token (applied for open accounts)
 
-    friends_path = bfs(access_token, start_id=20067703, finish_id=)
-    print(*friends_path)
+    #friends_path, distance = bfs(access_token, start_id='20067703', finish_id=16109999)
+    #print(*friends_path)
+    #print(f'You are {distance} handshakes away from this person')
+    print(get_id(access_token, 'carrillo230502'))
+    print(type(get_id(access_token, 'olgabzova')))
+
 
 
 def get_friends_list(token, user_id):
@@ -27,21 +32,12 @@ def get_friends_list(token, user_id):
               'access_token': token,
               'v': 5.122}
 
-    try:
-        resp = requests.get(url, params)
-
-        # raise exceptions if error occurs
-        resp.raise_for_status()
-    except HTTPError as http_err:
-        print('HTTP error occurred: ', http_err)
-    except Exception as err:
-        print('Other error occurred', err)
+    resp = make_request(url, params)
+    resp = resp.json()
+    if 'response' in resp:  # check for correct response
+        return resp['response']['items']
     else:
-        resp = resp.json()
-        if 'response' in resp:  # check for correct response
-            return resp['response']['items']
-        else:
-            return []
+        return []
 
 
 def bfs(token, start_id, finish_id):
@@ -52,9 +48,10 @@ def bfs(token, start_id, finish_id):
     distances = {start_id: 0}
     parents = {start_id: None}
     queue = deque([start_id])
-    while finish_id not in distances:
+    while finish_id not in distances and queue:
         current_user = queue.popleft()
         friends_list = get_friends_list(token, current_user)
+        sleep(0.25)
         for user in tqdm(friends_list):
             if user not in distances:
                 distances[user] = distances[current_user] + 1
@@ -71,13 +68,46 @@ def bfs(token, start_id, finish_id):
         while parent is not None:
             path.append(parent)
             parent = parents[parent]
-        return path[::-1]
+        return path[::-1], distances[finish_id]
+
+
+def get_id(token, screen_name):
+    """Getting user id by his screen_name (domain name)"""
+
+    url = 'https://api.vk.com/method/utils.resolveScreenName'
+    params = {
+        'screen_name': screen_name,
+        'access_token': token,
+        'v': 5.122
+    }
+    resp = make_request(url, params)
+    resp = resp.json()
+    if 'object_id' in resp['response']:
+        return resp['response']['object_id']
+    else:
+        return None
+
+
+def visualize_path(path):
+    """Man-understandable representation of friends path
+            (first_name, last_name and link)"""
+
+
+def make_request(url, params=None):
+    """Make Http Request"""
+    try:
+        resp = requests.get(url, params)
+
+        # raise exceptions if error occurs
+        resp.raise_for_status()
+    except HTTPError as http_err:
+        print('HTTP error occurred: ', http_err)
+    except Exception as err:
+        print('Other error occurred', err)
+    else:
+        return resp
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
 
